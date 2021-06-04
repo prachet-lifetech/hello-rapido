18075f03e7e0eac30a622162c2fc4ffdb23cd618
Amit Kumar <amitgarg.cse@gmail.com>
2021-06-04 08:24:52 +0530

    Initial working checkin

---
+++ android/app/src/main/kotlin/com/prachet/hello_rapido/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.prachet.hello_rapido
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:rapido/rapido.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DocumentList documentList = DocumentList("Tasker", labels: {
+    "Date": "date",
+    "Task": "title",
+    "Priority": "pri count",
+    "Note": "subtitle"
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return DocumentListScaffold(documentList);
+  }
+}
